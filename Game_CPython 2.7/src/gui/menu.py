a046b4b7d17fc244be9e33d0b4b5401a855c0c1f
Morgan Cromell <morgan.cromell@elev.it-gymnasiet.se>
2014-01-13 22:59:12 +0100

    Started working on a sub menu system.

--- Game_CPython 2.7/src/gui/menu.py
+++ Game_CPython 2.7/src/gui/menu.py
@@ -9,39 +9,68 @@ class MenuItem(object):
         self.rect = rect
         self.active = False
         self.data = data
-        self.image = render_textrect(str(self.data), 30, self.rect, (255, 255, 255))
-        self.active_image = render_textrect(str(self.data), 30, self.rect, (0, 255, 0))
+        self.image = render_textrect(str(self.data), 30, self.rect, (128, 128, 128), None, 1)
+        self.active_image = render_textrect(str(self.data), 30, self.rect, (255, 255, 255), None, 1)
 
 
 class Menu(Gui):
     def __init__(self, middle, options):
         self.options = options
-        width = 15 * len(max(options, key=len))
-        height = 30*len(options)
-        image = pygame.Surface((width, height))
-        self.rect = pygame.Rect((middle[0] - width/2, middle[1] - height/2), (width, height))
+        self.middle = middle
+        self.width = 15 * len(max(options, key=len))
+        self.height = 30*len(options)
+        self.main = True
+        self.main_bg = pygame.image.load('res/gui/main_menu_bg.png')
+        self.main_bg = pygame.transform.scale(self.main_bg, (self.middle[0]*2, self.middle[1]*2))
+        image = pygame.Surface((self.width, self.height), pygame.SRCALPHA, 32)
+        image.convert_alpha()
+        self.rect = pygame.Rect((middle[0] - self.width/2, self.middle[1] - self.height/2), (self.width, self.height))
         self.items = []
+        self.logo_rect = pygame.Rect((0, 0), (250, 50))
+        self.title = render_textrect(' ', 30, self.logo_rect, (255, 255, 0), None, 1)
+        self.logo = render_textrect('Dungeon of Doom (Alpha)', 30, self.logo_rect, (255, 255, 0), None, 1)
         Gui.__init__(self, 'menu', (self.rect.x, self.rect.y), image, True)
 
         for key, option in enumerate(self.options):
-            rect = pygame.Rect((0, 0 + (30 * key)), (15*len(option), 30))
+            rect = pygame.Rect((0, 0 + (30 * key)), (self.width, 30))
+            self.items.append(MenuItem(rect, option))
+
+    def change_title(self, string):
+        self.title = render_textrect(string, 30, self.logo_rect, (255, 255, 0), None, 1)
+
+    def change_options(self, options):
+        self.options = options
+        self.items = []
+        for key, option in enumerate(self.options):
+            rect = pygame.Rect((0, 0 + (30 * key)), (self.width, 30))
             self.items.append(MenuItem(rect, option))
 
     def update(self, game):
         for item in self.items:
-            if item.rect.collidepoint(pygame.mouse.get_pos()[0] - self.x, pygame.mouse.get_pos()[1] - self.y):
+            if item.rect.collidepoint(pygame.mouse.get_pos()[0] - self.x, pygame.mouse.get_pos()[1] - self.y) and item.data in self.options:
                 item.active = True
                 if pygame.mouse.get_pressed()[0]:
                     if item.data == 'New Game':
-                        game.new_game()
+                        self.items = []
+                        self.change_title('Select class')
+                        self.change_options(['Warrior', 'Mage', 'Ranger'])
+                        #game.new_game()
                     if item.data == 'Quit':
                         pygame.quit()
                         sys.exit()
-                    self.active = False
+                    #self.active = False
+                    #self.main = False
             else:
                 item.active = False
 
     def draw(self, screen):
+        self.image.fill((0, 0, 0))
+        if self.main:
+            self.image.blit(self.main_bg, (-self.x, -self.y))
+            screen.blit(self.main_bg, (0, 0))
+            screen.blit(self.logo, (10 + self.middle[0] - self.logo_rect.w/2, 100))
+            if self.title:
+                screen.blit(self.title, (self.middle[0] - self.logo_rect.w/2, 200))
         for item in self.items:
             if item.active:
                 self.image.blit(item.active_image, (item.rect.x, item.rect.y))
